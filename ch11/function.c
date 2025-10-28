7c4faadd66469858a2f275c88ebb21b024b47572
jihuni0105 <jkhslove80@gmail.com>
2025-10-28 14:54:52 +0900

    .

--- ch11/function.c
+++ ch11/function.c
@@ -2,13 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 
+
 //함수명: get_id()
 //기능:1001번부터 차례로 유일한 번호를 증가하면서 반환하는 기능
 //입력:없음
 //반환값:1001부터 시작해서 1씩 증가하는 유일한 값
 //오류:없음
 
-static int inc_id(int n);
 static int inc_id(int n);
 
 static int get_id(void)
--- ch11/main.c
+++ ch11/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+#include <stdlib.h> 
 
 extern int get_id(void);
 extern int last_id;
